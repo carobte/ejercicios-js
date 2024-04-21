68e5fd3f30f9194841b86b10c1cfde8858a635b3
carobte <caro.bustamante.escobar@gmail.com>
2024-04-20 21:09:51 -0500

    enunciados operadores

--- src/js/4Operadores.js
+++ src/js/4Operadores.js
@@ -1,62 +1,123 @@
 console.log("Ejercicios operadores")
 
+// 1. ¿Es 35 mayor que 25?
+
 console.log(`1. ${35 > 25}`)
+
+// 2. ¿Es 45 menor que 30?
 
 console.log(`2. ${45 < 30}`)
 
+// 3. ¿Son 50 y "50" iguales?
+
 console.log(`3. ${50 == "50"}`)
+
+// 4. ¿Es 40 estrictamente igual a 40 convertido a número?
 
 console.log(`4. ${40 === Number(40)}`)
 
+// 5. ¿Son "apple" y "orange" diferentes?
+
 console.log(`5. ${"apple" != "orange"}`)
 
+// 6. ¿Es "apple" estrictamente diferente de "Apple"?
+
 console.log(`6. ${"apple" === "Apple"}`)
+
+// 7. ¿Cuál es el valor de una variable sin definir?
 
 let a
 console.log(`7. ${a}`)
 
+// 8. ¿Cuál es el tipo de dato de una variable sin definir?
+
 console.log(`8. ${typeof a}`)
+
+// 9. ¿Es null igual a undefined?
 
 console.log(`9. ${null == undefined}`) //da true porque ambos son falsy 
 
+// 10. ¿Son null y undefined estrictamente iguales?
+
 console.log(`10. ${null === undefined}`)
+
+// 11. ¿Es "2.75" mayor que 2.5?
 
 console.log(`11. ${"2.75" > 2.5}`)
 
+// 12. ¿Es "2.75" mayor que 2.75?
+
 console.log(`12. ${"2.75" > 2.75}`)
+
+// 13. ¿Es "2.75" mayor o igual a 2.75?
 
 console.log(`13. ${"2.75" >= 2.75}`)
 
+// 14. ¿Es "2.75" menor o igual a 2.75?
+
 console.log(`14. ${"2.75" <= 2.75}`)
+
+// 15. ¿Es "2.75" un número mayor a 2.8?
 
 console.log(`15. ${"2.75" > 2.8}`)
 
+// 16. ¿Es "2.75" un número menor a 2.7?
+
 console.log(`16. ${"2.75" < 2.7}`)
+
+// 17. ¿Es el nombre "John" diferente de "john"?
 
 console.log(`17. ${"John" != "john"}`)
 
+// 18. ¿Es el nombre "John" diferente de "Jon"?
+
 console.log(`18. ${"John" != "Jon"}`)
+
+// 19. ¿Es la edad 30 estrictamente diferente de "30"?
 
 console.log(`19. ${30 !== "30"}`)
 
+// 20. ¿Cuál es el resultado de desplazar a la derecha 25 dos veces en bits?
+
 console.log(`20. ${25 >> 2}`)
+
+// 21. ¿Cuál es el resultado de desplazar a la izquierda 8 tres veces en bits?
 
 console.log(`21. ${8 << 3}`)
 
+// 22. ¿Son 15 y 15 iguales?
+
 console.log(`22. ${15 == 15}`)
+
+// 23. ¿Son 20 y 30 diferentes?
 
 console.log(`23. ${20 != 30} `)
 
+// 24. ¿Es "15" mayor que 30?
+
 console.log(`24. ${"15" > 30} `)
+
+// 25. ¿Es 20 menor que 40?
 
 console.log(`25. ${20 < 40} `)
 
+// 26. ¿Es 100 mayor o igual a "99"?
+
 console.log(`26. ${100 >= "99"}`)
+
+// 27. ¿Es 500 estrictamente igual a "500"?
 
 console.log(`27. ${500 === "500"}`)
 
+// 28. ¿Es 750 estrictamente igual a 750?
+
 console.log(`28. ${750 === 750}`)
+
+// 29. ¿Es 5 estrictamente diferente de "5"?
 
 console.log(`29. ${5 !== "5"}`)
 
+// 30. ¿Es 3 mayor que 5 y 4 menor que 6? (Operadores lógicos)
+
 console.log(`30. ${3 > 5 && 4 < 6}`)
+
